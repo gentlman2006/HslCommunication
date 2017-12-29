2a82a923793d3f5988208f9250bda98effc56a87
dathlin <hsl200909@163.com>
2017-12-29 09:01:23 +0800

    饼图文字的位置调整，根据字体大小自动变化。

--- HslCommunication_Net35/Controls/UserPieChart.cs
+++ HslCommunication_Net35/Controls/UserPieChart.cs
@@ -20,20 +20,20 @@ namespace HslCommunication.Controls
         /// <summary>
         /// 实例化一个饼图的控件
         /// </summary>
-        public UserPieChart()
+        public UserPieChart( )
         {
-            InitializeComponent();
-            random = new Random();
+            InitializeComponent( );
+            random = new Random( );
             DoubleBuffered = true;
 
-            formatCenter = new StringFormat();
+            formatCenter = new StringFormat( );
             formatCenter.Alignment = StringAlignment.Center;
             formatCenter.LineAlignment = StringAlignment.Center;
 
             pieItems = new HslPieItem[0];
         }
 
-        private void UserPieChart_Load(object sender, EventArgs e)
+        private void UserPieChart_Load( object sender, EventArgs e )
         {
 
         }
@@ -48,18 +48,18 @@ namespace HslCommunication.Controls
         /// <summary>
         /// 是否显示百分比信息
         /// </summary>
-        [Browsable(true)]
-        [Category("外观")]
-        [DefaultValue(false)]
-        [Description("获取或设置是否显示百分比占用")]
+        [Browsable( true )]
+        [Category( "外观" )]
+        [DefaultValue( false )]
+        [Description( "获取或设置是否显示百分比占用" )]
         public bool IsRenderPercent
         {
             get { return m_IsRenderPercent; }
-            set { m_IsRenderPercent = value; Invalidate(); }
+            set { m_IsRenderPercent = value; Invalidate( ); }
         }
 
 
-        private void SetMarginPaint(int value)
+        private void SetMarginPaint( int value )
         {
             if (value > 500)
             {
@@ -75,19 +75,19 @@ namespace HslCommunication.Controls
             }
         }
 
-        private Point GetCenterPoint(out int width)
+        private Point GetCenterPoint( out int width )
         {
             if (Width > Height)
             {
-                SetMarginPaint(Height);
+                SetMarginPaint( Height );
                 width = Height / 2 - margin;
-                return new Point(Height / 2 - 1, Height / 2 - 1);
+                return new Point( Height / 2 - 1, Height / 2 - 1 );
             }
             else
             {
-                SetMarginPaint(Width);
+                SetMarginPaint( Width );
                 width = Width / 2 - margin;
-                return new Point(Width / 2 - 1, Width / 2 - 1);
+                return new Point( Width / 2 - 1, Width / 2 - 1 );
             }
         }
 
@@ -96,38 +96,38 @@ namespace HslCommunication.Controls
         /// 随机生成颜色，该颜色相对于白色为深色颜色
         /// </summary>
         /// <returns></returns>
-        private Color GetRandomColor()
+        private Color GetRandomColor( )
         {
-            int int_Red = random.Next(256);
-            int int_Green = random.Next(256);
-            int int_Blue = (int_Red + int_Green > 430) ? random.Next(100) : random.Next(200);
-            return Color.FromArgb(int_Red, int_Green, int_Blue);
+            int int_Red = random.Next( 256 );
+            int int_Green = random.Next( 256 );
+            int int_Blue = (int_Red + int_Green > 430) ? random.Next( 100 ) : random.Next( 200 );
+            return Color.FromArgb( int_Red, int_Green, int_Blue );
         }
 
 
-        private void UserPieChart_Paint(object sender, PaintEventArgs e)
+        private void UserPieChart_Paint( object sender, PaintEventArgs e )
         {
             // 画刷初始化
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
             e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
 
             // 开始绘制饼图，寻找中心点
-            Point center = GetCenterPoint(out int width);
-            Rectangle rect_center = new Rectangle(center.X - width, center.Y - width, width + width, width + width);
+            Point center = GetCenterPoint( out int width );
+            Rectangle rect_center = new Rectangle( center.X - width, center.Y - width, width + width, width + width );
 
             if (width > 0 && pieItems.Length > 0)
             {
-                e.Graphics.FillEllipse(Brushes.AliceBlue, rect_center);
-                e.Graphics.DrawEllipse(Pens.DodgerBlue, rect_center);
+                e.Graphics.FillEllipse( Brushes.AliceBlue, rect_center );
+                e.Graphics.DrawEllipse( Pens.DodgerBlue, rect_center );
 
-                Rectangle rect_tran = new Rectangle(rect_center.X - center.X, rect_center.Y - center.Y, rect_center.Width, rect_center.Height);
-                e.Graphics.TranslateTransform(center.X, center.Y);
-                e.Graphics.RotateTransform(90);
-                e.Graphics.DrawLine(Pens.DimGray, 0, 0, width, 0);
+                Rectangle rect_tran = new Rectangle( rect_center.X - center.X, rect_center.Y - center.Y, rect_center.Width, rect_center.Height );
+                e.Graphics.TranslateTransform( center.X, center.Y );
+                e.Graphics.RotateTransform( 90 );
+                e.Graphics.DrawLine( Pens.DimGray, 0, 0, width, 0 );
 
 
                 // 计算分布
-                int totle = pieItems.Sum(item => item.Value);
+                int totle = pieItems.Sum( item => item.Value );
                 float totleAngle = 0;     // 本次绘制的角度
                 float lastAngle = -90;    // 上一次的角度
                 for (int i = 0; i < pieItems.Length; i++)
@@ -139,15 +139,15 @@ namespace HslCommunication.Controls
                     }
                     else
                     {
-                        single = Convert.ToSingle(pieItems[i].Value * 1.0d / totle * 360);
+                        single = Convert.ToSingle( pieItems[i].Value * 1.0d / totle * 360 );
                     }
 
 
-                    using (Brush brush = new SolidBrush(pieItems[i].Back))
+                    using (Brush brush = new SolidBrush( pieItems[i].Back ))
                     {
-                        e.Graphics.FillPie(brush, rect_tran, 0, -single);
+                        e.Graphics.FillPie( brush, rect_tran, 0, -single );
                     }
-                    e.Graphics.RotateTransform(0 - single / 2);
+                    e.Graphics.RotateTransform( 0 - single / 2 );
 
                     // 画标线
                     totleAngle += single / 2;
@@ -155,10 +155,10 @@ namespace HslCommunication.Controls
                     if (totleAngle < 45 || totleAngle > 315) mark = 15;
                     if (totleAngle > 135 && totleAngle < 225) mark = 15;
 
-                    e.Graphics.DrawLine(Pens.DimGray, width * 2 / 3, 0, width + mark, 0);
-                    e.Graphics.TranslateTransform(width + mark, 0);
+                    e.Graphics.DrawLine( Pens.DimGray, width * 2 / 3, 0, width + mark, 0 );
+                    e.Graphics.TranslateTransform( width + mark, 0 );
 
-                    if(totleAngle - lastAngle < 5)
+                    if (totleAngle - lastAngle < 5)
                     {
                         // 两次的标线非常接近
 
@@ -167,59 +167,59 @@ namespace HslCommunication.Controls
 
                     if (totleAngle < 90)
                     {
-                        e.Graphics.RotateTransform(totleAngle - 90);
-                        e.Graphics.DrawLine(Pens.DimGray, 0, 0, margin - mark, 0);
-                        e.Graphics.DrawString(pieItems[i].Name, Font, Brushes.DimGray, new Point(0, -16));
-                        if (IsRenderPercent) e.Graphics.DrawString(Math.Round(single * 100 / 360, 2).ToString() + "%", Font, Brushes.DodgerBlue, new Point(0, 1));
-                        e.Graphics.RotateTransform(90 - totleAngle);
+                        e.Graphics.RotateTransform( totleAngle - 90 );
+                        e.Graphics.DrawLine( Pens.DimGray, 0, 0, margin - mark, 0 );
+                        e.Graphics.DrawString( pieItems[i].Name, Font, Brushes.DimGray, new Point( 0, -Font.Height ) );
+                        if (IsRenderPercent) e.Graphics.DrawString( Math.Round( single * 100 / 360, 2 ).ToString( ) + "%", Font, Brushes.DodgerBlue, new Point( 0, 1 ) );
+                        e.Graphics.RotateTransform( 90 - totleAngle );
                     }
                     else if (totleAngle < 180)
                     {
-                        e.Graphics.RotateTransform(totleAngle - 90);
-                        e.Graphics.DrawLine(Pens.DimGray, 0, 0, margin - mark, 0);
-                        e.Graphics.DrawString(pieItems[i].Name, Font, Brushes.DimGray, new Point(0, -16));
-                        if (IsRenderPercent) e.Graphics.DrawString(Math.Round(single * 100 / 360, 2).ToString() + "%", Font, Brushes.DodgerBlue, new Point(0, 1));
-                        e.Graphics.RotateTransform(90 - totleAngle);
+                        e.Graphics.RotateTransform( totleAngle - 90 );
+                        e.Graphics.DrawLine( Pens.DimGray, 0, 0, margin - mark, 0 );
+                        e.Graphics.DrawString( pieItems[i].Name, Font, Brushes.DimGray, new Point( 0, -Font.Height ) );
+                        if (IsRenderPercent) e.Graphics.DrawString( Math.Round( single * 100 / 360, 2 ).ToString( ) + "%", Font, Brushes.DodgerBlue, new Point( 0, 1 ) );
+                        e.Graphics.RotateTransform( 90 - totleAngle );
                     }
                     else if (totleAngle < 270)
                     {
-                        e.Graphics.RotateTransform(totleAngle - 270);
-                        e.Graphics.DrawLine(Pens.DimGray, 0, 0, margin - mark, 0);
-                        e.Graphics.TranslateTransform(margin-8, 0);
-                        e.Graphics.RotateTransform(180);
-                        e.Graphics.DrawString(pieItems[i].Name, Font, Brushes.DimGray, new Point(0, -16));
-                        if (IsRenderPercent) e.Graphics.DrawString(Math.Round(single * 100 / 360, 2).ToString() + "%", Font, Brushes.DodgerBlue, new Point(0, 1));
-                        e.Graphics.RotateTransform(-180);
-                        e.Graphics.TranslateTransform(8-margin, 0);
-                        e.Graphics.RotateTransform(270 - totleAngle);
+                        e.Graphics.RotateTransform( totleAngle - 270 );
+                        e.Graphics.DrawLine( Pens.DimGray, 0, 0, margin - mark, 0 );
+                        e.Graphics.TranslateTransform( margin - 8, 0 );
+                        e.Graphics.RotateTransform( 180 );
+                        e.Graphics.DrawString( pieItems[i].Name, Font, Brushes.DimGray, new Point( 0, -Font.Height ) );
+                        if (IsRenderPercent) e.Graphics.DrawString( Math.Round( single * 100 / 360, 2 ).ToString( ) + "%", Font, Brushes.DodgerBlue, new Point( 0, 1 ) );
+                        e.Graphics.RotateTransform( -180 );
+                        e.Graphics.TranslateTransform( 8 - margin, 0 );
+                        e.Graphics.RotateTransform( 270 - totleAngle );
                     }
                     else
                     {
-                        e.Graphics.RotateTransform(totleAngle - 270);
-                        e.Graphics.DrawLine(Pens.DimGray, 0, 0, margin - mark, 0);
-                        e.Graphics.TranslateTransform(margin - 8, 0);
-                        e.Graphics.RotateTransform(180);
-                        e.Graphics.DrawString(pieItems[i].Name, Font, Brushes.DimGray, new Point(0, -16));
-                        if (IsRenderPercent) e.Graphics.DrawString(Math.Round(single * 100 / 360, 2).ToString() + "%", Font, Brushes.DodgerBlue, new Point(0, 1));
-                        e.Graphics.RotateTransform(-180);
-                        e.Graphics.TranslateTransform(8 - margin, 0);
-                        e.Graphics.RotateTransform(270 - totleAngle);
+                        e.Graphics.RotateTransform( totleAngle - 270 );
+                        e.Graphics.DrawLine( Pens.DimGray, 0, 0, margin - mark, 0 );
+                        e.Graphics.TranslateTransform( margin - 8, 0 );
+                        e.Graphics.RotateTransform( 180 );
+                        e.Graphics.DrawString( pieItems[i].Name, Font, Brushes.DimGray, new Point( 0, -Font.Height ) );
+                        if (IsRenderPercent) e.Graphics.DrawString( Math.Round( single * 100 / 360, 2 ).ToString( ) + "%", Font, Brushes.DodgerBlue, new Point( 0, 1 ) );
+                        e.Graphics.RotateTransform( -180 );
+                        e.Graphics.TranslateTransform( 8 - margin, 0 );
+                        e.Graphics.RotateTransform( 270 - totleAngle );
                     }
 
-                    e.Graphics.TranslateTransform(-width - mark, 0);
-                    e.Graphics.RotateTransform(0 - single / 2);
+                    e.Graphics.TranslateTransform( -width - mark, 0 );
+                    e.Graphics.RotateTransform( 0 - single / 2 );
                     totleAngle += single / 2;
                 }
 
 
 
-                e.Graphics.ResetTransform();
+                e.Graphics.ResetTransform( );
             }
             else
             {
-                e.Graphics.FillEllipse(Brushes.AliceBlue, rect_center);
-                e.Graphics.DrawEllipse(Pens.DodgerBlue, rect_center);
-                e.Graphics.DrawString("空", Font, Brushes.DimGray, rect_center, formatCenter);
+                e.Graphics.FillEllipse( Brushes.AliceBlue, rect_center );
+                e.Graphics.DrawEllipse( Pens.DodgerBlue, rect_center );
+                e.Graphics.DrawString( "空", Font, Brushes.DimGray, rect_center, formatCenter );
             }
         }
 
@@ -233,12 +233,12 @@ namespace HslCommunication.Controls
         /// </summary>
         /// <param name="source">特殊的显示对象</param>
         /// <exception cref="ArgumentNullException"></exception>
-        public void SetDataSource(HslPieItem[] source)
+        public void SetDataSource( HslPieItem[] source )
         {
             if (source != null)
             {
                 pieItems = source;
-                Invalidate();
+                Invalidate( );
             }
         }
 
@@ -248,25 +248,25 @@ namespace HslCommunication.Controls
         /// <param name="names">名称</param>
         /// <param name="values">值</param>
         /// <exception cref="ArgumentNullException"></exception>
-        public void SetDataSource(string[] names,int[] values)
+        public void SetDataSource( string[] names, int[] values )
         {
-            if (names == null) throw new ArgumentNullException("names");
-            if (values == null) throw new ArgumentNullException("values");
-            if (names.Length != values.Length) throw new Exception("两个数组的长度不一致！");
+            if (names == null) throw new ArgumentNullException( "names" );
+            if (values == null) throw new ArgumentNullException( "values" );
+            if (names.Length != values.Length) throw new Exception( "两个数组的长度不一致！" );
 
             pieItems = new HslPieItem[names.Length];
 
             for (int i = 0; i < names.Length; i++)
             {
-                pieItems[i] = new HslPieItem()
+                pieItems[i] = new HslPieItem( )
                 {
                     Name = names[i],
                     Value = values[i],
-                    Back = GetRandomColor(),
+                    Back = GetRandomColor( ),
                 };
             }
 
-            Invalidate();
+            Invalidate( );
         }
 
         #endregion
