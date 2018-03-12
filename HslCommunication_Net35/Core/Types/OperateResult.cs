d4d5f88b293ff56b1b1c8f24dc15a34a4501cc1b
dathlin <hsl200909@163.com>
2018-03-12 09:12:13 +0800

    结果类代码扩充完成，方便返回所有的成功对象，从带一个参数到全部参数。

--- HslCommunication_Net35/Core/Types/OperateResult.cs
+++ HslCommunication_Net35/Core/Types/OperateResult.cs
@@ -169,9 +169,9 @@ namespace HslCommunication
         /// <param name="value3">类型三对象</param>
         /// <param name="value4">类型四对象</param>
         /// <returns>成的结果对象</returns>
-        public static OperateResult<T1, T2, T3, T4> CreateSuccessResult<T1, T2, T3, T4>(T1 value1, T2 value2, T3 value3, T4 value4)
+        public static OperateResult<T1, T2, T3, T4> CreateSuccessResult<T1, T2, T3, T4>( T1 value1, T2 value2, T3 value3, T4 value4 )
         {
-            return new OperateResult<T1, T2, T3, T4>()
+            return new OperateResult<T1, T2, T3, T4>( )
             {
                 IsSuccess = true,
                 Message = StringResources.SuccessText,
@@ -179,6 +179,222 @@ namespace HslCommunication
                 Content2 = value2,
                 Content3 = value3,
                 Content4 = value4,
+            };
+        }
+
+
+        /// <summary>
+        /// 创建并返回一个成功的结果对象，并带有五个参数对象
+        /// </summary>
+        /// <typeparam name="T1">第一个参数类型</typeparam>
+        /// <typeparam name="T2">第二个参数类型</typeparam>
+        /// <typeparam name="T3">第三个参数类型</typeparam>
+        /// <typeparam name="T4">第四个参数类型</typeparam>
+        /// <typeparam name="T5">第五个参数类型</typeparam>
+        /// <param name="value1">类型一对象</param>
+        /// <param name="value2">类型二对象</param>
+        /// <param name="value3">类型三对象</param>
+        /// <param name="value4">类型四对象</param>
+        /// <param name="value5">类型五对象</param>
+        /// <returns>成的结果对象</returns>
+        public static OperateResult<T1, T2, T3, T4, T5> CreateSuccessResult<T1, T2, T3, T4, T5>( T1 value1, T2 value2, T3 value3, T4 value4, T5 value5 )
+        {
+            return new OperateResult<T1, T2, T3, T4, T5>( )
+            {
+                IsSuccess = true,
+                Message = StringResources.SuccessText,
+                Content1 = value1,
+                Content2 = value2,
+                Content3 = value3,
+                Content4 = value4,
+                Content5 = value5,
+            };
+        }
+
+        /// <summary>
+        /// 创建并返回一个成功的结果对象，并带有六个参数对象
+        /// </summary>
+        /// <typeparam name="T1">第一个参数类型</typeparam>
+        /// <typeparam name="T2">第二个参数类型</typeparam>
+        /// <typeparam name="T3">第三个参数类型</typeparam>
+        /// <typeparam name="T4">第四个参数类型</typeparam>
+        /// <typeparam name="T5">第五个参数类型</typeparam>
+        /// <typeparam name="T6">第六个参数类型</typeparam>
+        /// <param name="value1">类型一对象</param>
+        /// <param name="value2">类型二对象</param>
+        /// <param name="value3">类型三对象</param>
+        /// <param name="value4">类型四对象</param>
+        /// <param name="value5">类型五对象</param>
+        /// <param name="value6">类型六对象</param>
+        /// <returns>成的结果对象</returns>
+        public static OperateResult<T1, T2, T3, T4, T5, T6> CreateSuccessResult<T1, T2, T3, T4, T5, T6>( T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6 )
+        {
+            return new OperateResult<T1, T2, T3, T4, T5, T6>( )
+            {
+                IsSuccess = true,
+                Message = StringResources.SuccessText,
+                Content1 = value1,
+                Content2 = value2,
+                Content3 = value3,
+                Content4 = value4,
+                Content5 = value5,
+                Content6 = value6,
+            };
+        }
+
+        /// <summary>
+        /// 创建并返回一个成功的结果对象，并带有七个参数对象
+        /// </summary>
+        /// <typeparam name="T1">第一个参数类型</typeparam>
+        /// <typeparam name="T2">第二个参数类型</typeparam>
+        /// <typeparam name="T3">第三个参数类型</typeparam>
+        /// <typeparam name="T4">第四个参数类型</typeparam>
+        /// <typeparam name="T5">第五个参数类型</typeparam>
+        /// <typeparam name="T6">第六个参数类型</typeparam>
+        /// <typeparam name="T7">第七个参数类型</typeparam>
+        /// <param name="value1">类型一对象</param>
+        /// <param name="value2">类型二对象</param>
+        /// <param name="value3">类型三对象</param>
+        /// <param name="value4">类型四对象</param>
+        /// <param name="value5">类型五对象</param>
+        /// <param name="value6">类型六对象</param>
+        /// <param name="value7">类型七对象</param>
+        /// <returns>成的结果对象</returns>
+        public static OperateResult<T1, T2, T3, T4, T5, T6, T7> CreateSuccessResult<T1, T2, T3, T4, T5, T6, T7>( T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7 )
+        {
+            return new OperateResult<T1, T2, T3, T4, T5, T6, T7>( )
+            {
+                IsSuccess = true,
+                Message = StringResources.SuccessText,
+                Content1 = value1,
+                Content2 = value2,
+                Content3 = value3,
+                Content4 = value4,
+                Content5 = value5,
+                Content6 = value6,
+                Content7 = value7,
+            };
+        }
+
+
+        /// <summary>
+        /// 创建并返回一个成功的结果对象，并带有八个参数对象
+        /// </summary>
+        /// <typeparam name="T1">第一个参数类型</typeparam>
+        /// <typeparam name="T2">第二个参数类型</typeparam>
+        /// <typeparam name="T3">第三个参数类型</typeparam>
+        /// <typeparam name="T4">第四个参数类型</typeparam>
+        /// <typeparam name="T5">第五个参数类型</typeparam>
+        /// <typeparam name="T6">第六个参数类型</typeparam>
+        /// <typeparam name="T7">第七个参数类型</typeparam>
+        /// <typeparam name="T8">第八个参数类型</typeparam>
+        /// <param name="value1">类型一对象</param>
+        /// <param name="value2">类型二对象</param>
+        /// <param name="value3">类型三对象</param>
+        /// <param name="value4">类型四对象</param>
+        /// <param name="value5">类型五对象</param>
+        /// <param name="value6">类型六对象</param>
+        /// <param name="value7">类型七对象</param>
+        /// <param name="value8">类型八对象</param>
+        /// <returns>成的结果对象</returns>
+        public static OperateResult<T1, T2, T3, T4, T5, T6, T7, T8> CreateSuccessResult<T1, T2, T3, T4, T5, T6, T7, T8>( T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8 )
+        {
+            return new OperateResult<T1, T2, T3, T4, T5, T6, T7, T8>( )
+            {
+                IsSuccess = true,
+                Message = StringResources.SuccessText,
+                Content1 = value1,
+                Content2 = value2,
+                Content3 = value3,
+                Content4 = value4,
+                Content5 = value5,
+                Content6 = value6,
+                Content7 = value7,
+                Content8 = value8,
+            };
+        }
+
+
+        /// <summary>
+        /// 创建并返回一个成功的结果对象，并带有九个参数对象
+        /// </summary>
+        /// <typeparam name="T1">第一个参数类型</typeparam>
+        /// <typeparam name="T2">第二个参数类型</typeparam>
+        /// <typeparam name="T3">第三个参数类型</typeparam>
+        /// <typeparam name="T4">第四个参数类型</typeparam>
+        /// <typeparam name="T5">第五个参数类型</typeparam>
+        /// <typeparam name="T6">第六个参数类型</typeparam>
+        /// <typeparam name="T7">第七个参数类型</typeparam>
+        /// <typeparam name="T8">第八个参数类型</typeparam>
+        /// <typeparam name="T9">第九个参数类型</typeparam>
+        /// <param name="value1">类型一对象</param>
+        /// <param name="value2">类型二对象</param>
+        /// <param name="value3">类型三对象</param>
+        /// <param name="value4">类型四对象</param>
+        /// <param name="value5">类型五对象</param>
+        /// <param name="value6">类型六对象</param>
+        /// <param name="value7">类型七对象</param>
+        /// <param name="value8">类型八对象</param>
+        /// <param name="value9">类型九对象</param>
+        /// <returns>成的结果对象</returns>
+        public static OperateResult<T1, T2, T3, T4, T5, T6, T7, T8, T9> CreateSuccessResult<T1, T2, T3, T4, T5, T6, T7, T8, T9>( T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8, T9 value9 )
+        {
+            return new OperateResult<T1, T2, T3, T4, T5, T6, T7, T8, T9>( )
+            {
+                IsSuccess = true,
+                Message = StringResources.SuccessText,
+                Content1 = value1,
+                Content2 = value2,
+                Content3 = value3,
+                Content4 = value4,
+                Content5 = value5,
+                Content6 = value6,
+                Content7 = value7,
+                Content8 = value8,
+                Content9 = value9,
+            };
+        }
+
+        /// <summary>
+        /// 创建并返回一个成功的结果对象，并带有十个参数对象
+        /// </summary>
+        /// <typeparam name="T1">第一个参数类型</typeparam>
+        /// <typeparam name="T2">第二个参数类型</typeparam>
+        /// <typeparam name="T3">第三个参数类型</typeparam>
+        /// <typeparam name="T4">第四个参数类型</typeparam>
+        /// <typeparam name="T5">第五个参数类型</typeparam>
+        /// <typeparam name="T6">第六个参数类型</typeparam>
+        /// <typeparam name="T7">第七个参数类型</typeparam>
+        /// <typeparam name="T8">第八个参数类型</typeparam>
+        /// <typeparam name="T9">第九个参数类型</typeparam>
+        /// <typeparam name="T10">第十个参数类型</typeparam>
+        /// <param name="value1">类型一对象</param>
+        /// <param name="value2">类型二对象</param>
+        /// <param name="value3">类型三对象</param>
+        /// <param name="value4">类型四对象</param>
+        /// <param name="value5">类型五对象</param>
+        /// <param name="value6">类型六对象</param>
+        /// <param name="value7">类型七对象</param>
+        /// <param name="value8">类型八对象</param>
+        /// <param name="value9">类型九对象</param>
+        /// <param name="value10">类型十对象</param>
+        /// <returns>成的结果对象</returns>
+        public static OperateResult<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> CreateSuccessResult<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>( T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8, T9 value9, T10 value10 )
+        {
+            return new OperateResult<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>( )
+            {
+                IsSuccess = true,
+                Message = StringResources.SuccessText,
+                Content1 = value1,
+                Content2 = value2,
+                Content3 = value3,
+                Content4 = value4,
+                Content5 = value5,
+                Content6 = value6,
+                Content7 = value7,
+                Content8 = value8,
+                Content9 = value9,
+                Content10 = value10,
             };
         }
 
