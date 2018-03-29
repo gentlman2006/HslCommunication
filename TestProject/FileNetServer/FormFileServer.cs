0add815e50ce11ba0af46d0f4556c52045a68ded
dathlin <hsl200909@163.com>
2018-03-29 10:59:42 +0800

    文件服务器编写完成

--- TestProject/FileNetServer/FormFileServer.cs
+++ TestProject/FileNetServer/FormFileServer.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using HslCommunication.Enthernet;
 
 namespace FileNetServer
 {
@@ -15,5 +16,129 @@ namespace FileNetServer
         {
             InitializeComponent( );
         }
+
+        private void FormFileServer_Load( object sender, EventArgs e )
+        {
+            textBox2.Text = Guid.Empty.ToString( );
+            textBox9.Text = Guid.Empty.ToString( );
+
+            textBox3.Text = Application.StartupPath + "\\AdvancedFiles";
+            textBox4.Text = Application.StartupPath + "\\FileTemp";
+
+            textBox8.Text = Application.StartupPath + "\\UltimateFiles";
+        }
+
+        private void linkLabel1_LinkClicked( object sender, LinkLabelLinkClickedEventArgs e )
+        {
+            try
+            {
+                System.Diagnostics.Process.Start( linkLabel1.Text );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message );
+            }
+        }
+
+        private void linkLabel2_LinkClicked( object sender, LinkLabelLinkClickedEventArgs e )
+        {
+            HslCommunication.BasicFramework.FormSupport form = new HslCommunication.BasicFramework.FormSupport( );
+            form.ShowDialog( );
+        }
+
+        #region Advanced Server
+
+        private AdvancedFileServer advancedFileServer;
+
+        private void AdvancedFileServerStart( )
+        {
+            if (!int.TryParse( textBox1.Text, out int port ))
+            {
+                MessageBox.Show( "Advanced文件服务器引擎的端口号输入异常" );
+            }
+
+            advancedFileServer = new AdvancedFileServer( );
+            advancedFileServer.FilesDirectoryPath = textBox3.Text;
+            advancedFileServer.FilesDirectoryPathTemp = textBox4.Text;
+            advancedFileServer.Token = new Guid( textBox2.Text );
+            advancedFileServer.LogNet = new HslCommunication.LogNet.LogNetSingle( Application.StartupPath + "\\Logs\\AdvancedLog.txt" );
+            advancedFileServer.LogNet.BeforeSaveToFile += LogNet1_BeforeSaveToFile;
+            advancedFileServer.ServerStart( port );
+        }
+
+        private void LogNet1_BeforeSaveToFile( object sender, HslCommunication.LogNet.HslEventArgs e )
+        {
+            if(InvokeRequired)
+            {
+                Invoke( new Action<object, HslCommunication.LogNet.HslEventArgs>( LogNet1_BeforeSaveToFile ), sender, e );
+                return;
+            }
+
+            textBox5.AppendText( e.HslMessage.ToString( ) + Environment.NewLine );
+        }
+        private void button1_Click( object sender, EventArgs e )
+        {
+            try
+            {
+                AdvancedFileServerStart( );
+                button1.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                HslCommunication.BasicFramework.SoftBasic.ShowExceptionMessage( ex );
+            }
+        }
+
+        #endregion
+
+        #region Ultimate Server
+
+
+
+        private UltimateFileServer ultimateFileServer;
+
+        private void UltimateFileServerStart( )
+        {
+            if (!int.TryParse( textBox10.Text, out int port ))
+            {
+                MessageBox.Show( "Advanced文件服务器引擎的端口号输入异常" );
+            }
+
+            ultimateFileServer = new UltimateFileServer( );
+            ultimateFileServer.FilesDirectoryPath = textBox8.Text;
+            ultimateFileServer.Token = new Guid( textBox9.Text );
+            ultimateFileServer.LogNet = new HslCommunication.LogNet.LogNetSingle( Application.StartupPath + "\\Logs\\UltimateLog.txt" );
+            ultimateFileServer.LogNet.BeforeSaveToFile += LogNet2_BeforeSaveToFile;
+            ultimateFileServer.ServerStart( port );
+        }
+
+        private void LogNet2_BeforeSaveToFile( object sender, HslCommunication.LogNet.HslEventArgs e )
+        {
+            if (InvokeRequired)
+            {
+                Invoke( new Action<object, HslCommunication.LogNet.HslEventArgs>( LogNet2_BeforeSaveToFile ), sender, e );
+                return;
+            }
+
+            textBox6.AppendText( e.HslMessage.ToString( ) + Environment.NewLine );
+        }
+        private void button2_Click( object sender, EventArgs e )
+        {
+            try
+            {
+                UltimateFileServerStart( );
+                button2.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                HslCommunication.BasicFramework.SoftBasic.ShowExceptionMessage( ex );
+            }
+        }
+
+
+
+        #endregion
+
+
     }
 }
