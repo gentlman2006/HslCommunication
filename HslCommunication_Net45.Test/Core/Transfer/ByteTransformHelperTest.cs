2a19b84ea7376c2fe1f7a930220575bd4123a71d
Richard.Hu <hsl200909@163.com>
2018-07-08 22:13:23 +0800

    单元测试新增ReverseBytesTransformTest测试

---
+++ HslCommunication_Net45.Test/Core/Transfer/ByteTransformHelperTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HslCommunication_Net45.Test.Core.Transfer
+{
+    [TestClass]
+    public class ByteTransformHelperTest
+    {
+
+
+
+
+
+
+    }
+}
