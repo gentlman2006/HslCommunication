5ebbf78445a627a1be9e98bde6985a00c7d60525
dathlin <hsl200909@163.com>
2018-07-08 17:59:15 +0800

    ByteTransformBase的double转换bug修复

--- HslCommunication_Net45/Core/Transfer/ByteTransformBase.cs
+++ HslCommunication_Net45/Core/Transfer/ByteTransformBase.cs
@@ -291,7 +291,7 @@ namespace HslCommunication.Core
             double[] tmp = new double[length];
             for (int i = 0; i < length; i++)
             {
-                tmp[i] = TransDouble( buffer, index + 4 * i );
+                tmp[i] = TransDouble( buffer, index + 8 * i );
             }
             return tmp;
         }
