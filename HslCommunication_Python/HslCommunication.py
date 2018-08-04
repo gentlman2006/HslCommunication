080e0675298233b3f1f279b96cd6ff4efae0364c
Richard.Hu <hsl200909@163.com>
2018-08-04 09:47:43 +0800

    feat(python): 继续完善代码。

--- HslCommunication_Python/HslCommunication.py
+++ HslCommunication_Python/HslCommunication.py
@@ -1,4 +1,6 @@
 import string
+import uuid
+import socket
 
 class StringResources:
 	'''系统的资源类'''
@@ -33,6 +35,30 @@ class OperateResult:
 	# 返回显示的文本
 	def ToMessageShowString(self):
 		return StringResources.ErrorCode() + ":" + str(self.ErrorCode) + "\r\n" + StringResources.TextDescription() + ":" + self.Message
+	def CreateFailedResult(msg):
+		'''创建一个失败的结果对象'''
+		failed = OperateResult()
+		failed.Message = msg
+		return failed
+	def CreateSuccessResult():
+		'''创建一个成功的对象'''
+		success = OperateResult()
+		success.IsSuccess = True
+		return success
+	def CreateSuccessResult(Content):
+		'''创建一个成功的结果对象，携带一个结果对象'''
+		success = OperateResult()
+		success.IsSuccess = True
+		success.Content = Content
+		return success
+	def CreateSuccessResult(Content1,Content2):
+		'''创建一个成功的结果对象，携带两个结果对象'''
+		success = OperateResult()
+		success.IsSuccess = True
+		success.Content1 = Content1
+		success.Content2 = Content2
+		return success
+
 
 
 class SoftBasic:
@@ -55,7 +81,27 @@ class SoftBasic:
 			str_list.append(hex(byte))
 		return segment.join(str_list)
 
-
+class NetworkBase:
+	Token = UUID('{00000000-0000-0000-0000-000000000000}')
+	CoreSocket = null
+	def Receive(self,socket,length):
+		totle = 0
+		data = ""
+		try:
+			while totle < length:
+				data += socket.Receive(length-totle)
+				totle += data.length
+			return OperateResult.CreateSuccessResult(data)
+		except Exception as e:
+			result = OperateResult()
+			result.Message = e.message
+			return result
+	def Send(self,socket,data):
+		try:
+			socket.send(data)
+			return OperateResult.CreateSuccessResult()
+		except Exception as e:
+			return OperateResult.CreateFailedResult(e.message)
 
 result = OperateResult()
 result.Content = 123
