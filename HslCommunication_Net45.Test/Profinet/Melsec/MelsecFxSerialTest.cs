0672443d59563b5e5c01348c16b8d1c2c95dba0a
dathlin <hsl200909@163.com>
2018-08-24 12:49:59 +0800

    test(melsec): 新增三菱串口类的指令生成测试。

--- HslCommunication_Net45.Test/Profinet/Melsec/MelsecFxSerialTest.cs
+++ HslCommunication_Net45.Test/Profinet/Melsec/MelsecFxSerialTest.cs
@@ -20,7 +20,7 @@ namespace HslCommunication_Net45.Test.Profinet.Melsec
             Assert.IsTrue( operateResult.IsSuccess, "Y23指令生成失败" );
             Assert.IsTrue( HslCommunication.BasicFramework.SoftBasic.IsTwoBytesEquel( corrent, operateResult.Content ), "Y23指令校验失败" );
 
-            corrent = new byte[] { 0x02, 0x37, 0x31, 0x31, 0x30, 0x38, 0x03, 0x30, 0x34 };
+            corrent = new byte[] { 0x02, 0x37, 0x31, 0x31, 0x30, 0x38, 0x03, 0x30, 0x34 }; // 02 37 31 31 30 38 03 30 34
             operateResult = HslCommunication.Profinet.Melsec.MelsecFxSerial.BuildWriteBoolPacket( "M17", true );
             Assert.IsTrue( operateResult.IsSuccess, "M17指令生成失败" );
             Assert.IsTrue( HslCommunication.BasicFramework.SoftBasic.IsTwoBytesEquel( corrent, operateResult.Content ), "M17指令校验失败" );
@@ -45,7 +45,25 @@ namespace HslCommunication_Net45.Test.Profinet.Melsec
             Assert.IsTrue( HslCommunication.BasicFramework.SoftBasic.IsTwoBytesEquel( corrent, read.Content ), "D123指令校验失败" );
         }
 
+        [TestMethod]
+        public void BuildReadBoolCommandTest( )
+        {
+            byte[] corrent = new byte[] { 0x02, 0x30, 0x30, 0x31, 0x30, 0x31, 0x30, 0x31, 0x03, 0x35, 0x36 }; // 02 30 30 31 30 31 30 31 03 35 36
+            OperateResult<byte[], int> operateResult = HslCommunication.Profinet.Melsec.MelsecFxSerial.BuildReadBoolCommand( "M14", 1 );
+            Assert.IsTrue( operateResult.IsSuccess, "M14指令生成失败" );
+            Assert.IsTrue( HslCommunication.BasicFramework.SoftBasic.IsTwoBytesEquel( corrent, operateResult.Content1 ), "M14指令校验失败" );
+;
+        }
 
+        [TestMethod]
+        public void BuildWriteWordCommandTest( )
+        {
+            byte[] corrent = new byte[] { 0x02, 0x31, 0x31, 0x30, 0x46, 0x36, 0x30, 0x32, 0x30, 0x43, 0x30, 0x30, 0x03, 0x34, 0x36 };
+            OperateResult<byte[]> operateResult = HslCommunication.Profinet.Melsec.MelsecFxSerial.BuildWriteWordCommand( "D123", BitConverter.GetBytes( (ushort)12 ) );
+            Assert.IsTrue( operateResult.IsSuccess, "D123写入指令生成失败" );
+            Assert.IsTrue( HslCommunication.BasicFramework.SoftBasic.IsTwoBytesEquel( corrent, operateResult.Content ), "123写入指令校验失败 : " + HslCommunication.BasicFramework.SoftBasic.ByteToHexString( operateResult.Content, ' ' ) );
+            
+        }
 
     }
 }
