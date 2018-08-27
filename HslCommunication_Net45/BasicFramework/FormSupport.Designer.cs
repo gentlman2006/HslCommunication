3c2210cd9e66fa9caa3d624bff0c0e139c3bca28
dathlin <hsl200909@163.com>
2018-08-27 15:39:03 +0800

    feat(support): 新增一个VIP技术支持群，针对打赏金额较多的老铁创建。

--- HslCommunication_Net45/BasicFramework/FormSupport.Designer.cs
+++ HslCommunication_Net45/BasicFramework/FormSupport.Designer.cs
@@ -35,6 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -69,7 +70,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(372, 493);
+            this.label4.Location = new System.Drawing.Point(371, 516);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(327, 17);
             this.label4.TabIndex = 4;
@@ -105,12 +106,23 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(32, 487);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(334, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "打赏超过100 Rmb的，可以申请VIP技术支持群：838185568";
+            // 
             // FormSupport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(722, 519);
+            this.ClientSize = new System.Drawing.Size(710, 542);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -139,5 +151,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
     }
 }
