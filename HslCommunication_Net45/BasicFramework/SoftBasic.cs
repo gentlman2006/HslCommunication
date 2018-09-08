f4b9802c6a38e14deb5a35c0610626dfd8f82071
dathlin <hsl200909@163.com>
2018-09-08 10:17:03 +0800

    feat(release): v5.3.0

--- HslCommunication_Net45/BasicFramework/SoftBasic.cs
+++ HslCommunication_Net45/BasicFramework/SoftBasic.cs
@@ -740,7 +740,7 @@ namespace HslCommunication.BasicFramework
         /// <remarks>
         /// 当你要显示本组件框架的版本号的时候，就可以用这个属性来显示
         /// </remarks>
-        public static SystemVersion FrameworkVersion { get; set; } = new SystemVersion( "5.2.3" );
+        public static SystemVersion FrameworkVersion { get; set; } = new SystemVersion( "5.3.0" );
 
 
         #endregion
