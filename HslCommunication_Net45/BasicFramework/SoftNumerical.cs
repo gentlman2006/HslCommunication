437a5ceb296eaaffd9a0949f53533c48621a06fe
dathlin <hsl200909@163.com>
2018-09-14 21:34:10 +0800

    feat(SoftNumerical): 新增IDispose接口及实现。

--- HslCommunication_Net45/BasicFramework/SoftNumerical.cs
+++ HslCommunication_Net45/BasicFramework/SoftNumerical.cs
@@ -187,7 +187,7 @@ namespace HslCommunication.BasicFramework
     /// <summary>
     /// 一个简单的不持久化的序号自增类，采用线程安全实现，并允许指定最大数字，到达后清空从指定数开始
     /// </summary>
-    public sealed class SoftIncrementCount
+    public sealed class SoftIncrementCount : IDisposable
     {
         #region Constructor
 
@@ -235,6 +235,50 @@ namespace HslCommunication.BasicFramework
 
             hybirdLock.Leave( );
             return value;
+        }
+
+
+        #endregion
+        
+        #region IDisposable Support
+
+        private bool disposedValue = false; // 要检测冗余调用
+
+        void Dispose( bool disposing )
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)。
+
+                    hybirdLock.Dispose( );
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
+                // TODO: 将大型字段设置为 null。
+
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
+        // ~SoftIncrementCount() {
+        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+        //   Dispose(false);
+        // }
+
+        // 添加此代码以正确实现可处置模式。
+        /// <summary>
+        /// 释放当前对象所占用的资源
+        /// </summary>
+        public void Dispose( )
+        {
+            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+            Dispose( true );
+            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
+            // GC.SuppressFinalize(this);
         }
 
         #endregion
