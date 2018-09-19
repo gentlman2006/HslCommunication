ba801142a3a77999d35742371013c458a7312871
dathlin <hsl200909@163.com>
2018-09-19 22:34:00 +0800

    style(softbasic): 注释新增english的版本。

--- HslCommunication_Net45/BasicFramework/SoftBasic.cs
+++ HslCommunication_Net45/BasicFramework/SoftBasic.cs
@@ -14,7 +14,8 @@ using System.Drawing;
 namespace HslCommunication.BasicFramework
 {
     /// <summary>
-    /// 一个软件基础类，提供常用的一些静态方法
+    /// 一个软件基础类，提供常用的一些静态方法 ->
+    /// A software-based class that provides some common static methods
     /// </summary>
     public class SoftBasic
     {
@@ -22,9 +23,9 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 获取文件的md5码
+        /// 获取文件的md5码 -> Get the MD5 code of the file
         /// </summary>
-        /// <param name="filePath">文件的路径，既可以是完整的路径，也可以是相对的路径</param>
+        /// <param name="filePath">文件的路径，既可以是完整的路径，也可以是相对的路径 -> The path to the file</param>
         /// <returns>Md5字符串</returns>
         /// <example>
         /// 下面举例实现获取一个文件的md5码
@@ -41,7 +42,7 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 获取数据流的md5码
+        /// 获取数据流的md5码 -> Get the MD5 code for the data stream
         /// </summary>
         /// <param name="stream">数据流，可以是内存流，也可以是文件流</param>
         /// <returns>Md5字符串</returns>
@@ -59,7 +60,7 @@ namespace HslCommunication.BasicFramework
 
 #if !NETSTANDARD2_0
         /// <summary>
-        /// 获取内存图片的md5码
+        /// 获取内存图片的md5码 -> Get the MD5 code of the memory picture
         /// </summary>
         /// <param name="bitmap">内存图片</param>
         /// <returns>Md5字符串</returns>
@@ -121,7 +122,8 @@ namespace HslCommunication.BasicFramework
         #region Array Expaned
 
         /// <summary>
-        /// 一个通用的数组新增个数方法，会自动判断越界情况，越界的情况下，会自动的截断或是填充
+        /// 一个通用的数组新增个数方法，会自动判断越界情况，越界的情况下，会自动的截断或是填充 -> 
+        /// A common array of new methods, will automatically determine the cross-border situation, in the case of cross-border, will be automatically truncated or filled
         /// </summary>
         /// <typeparam name="T">数据类型</typeparam>
         /// <param name="array">原数据</param>
@@ -181,7 +183,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 将一个数组进行扩充到指定长度，或是缩短到指定长度
+        /// 将一个数组进行扩充到指定长度，或是缩短到指定长度 ->
+        /// Extend an array to a specified length, or shorten to a specified length or fill
         /// </summary>
         /// <typeparam name="T">数组的类型</typeparam>
         /// <param name="data">原先数据的数据</param>
@@ -205,7 +208,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 将一个数组进行扩充到偶数长度
+        /// 将一个数组进行扩充到偶数长度 ->
+        /// Extend an array to even lengths
         /// </summary>
         /// <typeparam name="T">数组的类型</typeparam>
         /// <param name="data">原先数据的数据</param>
@@ -232,7 +236,8 @@ namespace HslCommunication.BasicFramework
         #region Byte Array compare
 
         /// <summary>
-        /// 判断两个字节的指定部分是否相同
+        /// 判断两个字节的指定部分是否相同 ->
+        /// Determines whether the specified portion of a two-byte is the same
         /// </summary>
         /// <param name="b1">第一个字节</param>
         /// <param name="start1">第一个字节的起始位置</param>
@@ -258,7 +263,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 判断两个字节的指定部分是否相同
+        /// 判断两个字节的指定部分是否相同 ->
+        /// Determines whether the specified portion of a two-byte is the same
         /// </summary>
         /// <param name="b1">第一个字节</param>
         /// <param name="b2">第二个字节</param>
@@ -275,7 +281,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 判断两个数据的令牌是否相等
+        /// 判断两个数据的令牌是否相等 ->
+        /// Determines whether the tokens of two data are equal
         /// </summary>
         /// <param name="head">字节数据</param>
         /// <param name="token">GUID数据</param>
@@ -290,7 +297,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 判断两个数据的令牌是否相等
+        /// 判断两个数据的令牌是否相等 ->
+        /// Determines whether the tokens of two data are equal
         /// </summary>
         /// <param name="token1">第一个令牌</param>
         /// <param name="token2">第二个令牌</param>
@@ -308,7 +316,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 获取一个枚举类型的所有枚举值，可直接应用于组合框数据
+        /// 获取一个枚举类型的所有枚举值，可直接应用于组合框数据 ->
+        /// Gets all the enumeration values of an enumeration type that can be applied directly to the combo box data
         /// </summary>
         /// <typeparam name="TEnum">枚举的类型值</typeparam>
         /// <returns>枚举值数组</returns>
@@ -325,7 +334,8 @@ namespace HslCommunication.BasicFramework
         #region JSON Data Get
 
         /// <summary>
-        /// 一个泛型方法，提供json对象的数据读取
+        /// 一个泛型方法，提供json对象的数据读取 ->
+        /// A generic method that provides data read for a JSON object
         /// </summary>
         /// <typeparam name="T">读取的泛型</typeparam>
         /// <param name="json">json对象</param>
@@ -350,7 +360,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 一个泛型方法，提供json对象的数据写入
+        /// 一个泛型方法，提供json对象的数据写入 ->
+        /// A generic method that provides data writing to a JSON object
         /// </summary>
         /// <typeparam name="T">写入的泛型</typeparam>
         /// <param name="json">json对象</param>
@@ -379,7 +390,8 @@ namespace HslCommunication.BasicFramework
 #if !NETSTANDARD2_0
 
         /// <summary>
-        /// 显示一个完整的错误信息
+        /// 显示一个完整的错误信息 ->
+        /// Displays a complete error message
         /// </summary>
         /// <param name="ex">异常对象</param>
         /// <remarks>调用本方法可以显示一个异常的详细信息</remarks>
@@ -391,7 +403,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 显示一个完整的错误信息，和额外的字符串描述信息
+        /// 显示一个完整的错误信息，和额外的字符串描述信息 ->
+        /// Displays a complete error message, and additional string description information
         /// </summary>
         /// <param name="extraMsg">额外的描述信息</param>
         /// <remarks>调用本方法可以显示一个异常的详细信息</remarks>
@@ -405,7 +418,8 @@ namespace HslCommunication.BasicFramework
 #endif
 
         /// <summary>
-        /// 获取一个异常的完整错误信息
+        /// 获取一个异常的完整错误信息 ->
+        /// Gets the complete error message for an exception
         /// </summary>
         /// <param name="ex">异常对象</param>
         /// <returns>完整的字符串数据</returns>
@@ -422,7 +436,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 获取一个异常的完整错误信息，和额外的字符串描述信息
+        /// 获取一个异常的完整错误信息，和额外的字符串描述信息 ->
+        /// Gets the complete error message for an exception, and additional string description information
         /// </summary>
         /// <param name="extraMsg">额外的信息</param>
         /// <param name="ex">异常对象</param>
@@ -450,7 +465,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 字节数据转化成16进制表示的字符串
+        /// 字节数据转化成16进制表示的字符串 ->
+        /// Byte data into a string of 16 binary representations
         /// </summary>
         /// <param name="InBytes">字节数组</param>
         /// <returns>返回的字符串</returns>
@@ -464,7 +480,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 字节数据转化成16进制表示的字符串
+        /// 字节数据转化成16进制表示的字符串 ->
+        /// Byte data into a string of 16 binary representations
         /// </summary>
         /// <param name="InBytes">字节数组</param>
         /// <param name="segment">分割符</param>
@@ -492,7 +509,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 字符串数据转化成16进制表示的字符串
+        /// 字符串数据转化成16进制表示的字符串 ->
+        /// String data into a string of 16 binary representations
         /// </summary>
         /// <param name="InString">输入的字符串数据</param>
         /// <returns>返回的字符串</returns>
@@ -509,7 +527,8 @@ namespace HslCommunication.BasicFramework
             };
 
         /// <summary>
-        /// 将16进制的字符串转化成Byte数据，将检测每2个字符转化，也就是说，中间可以是任意字符
+        /// 将16进制的字符串转化成Byte数据，将检测每2个字符转化，也就是说，中间可以是任意字符 ->
+        /// Converts a 16-character string into byte data, which will detect every 2 characters converted, that is, the middle can be any character
         /// </summary>
         /// <param name="hex">十六进制的字符串，中间可以是任意的分隔符</param>
         /// <returns>转换后的字节数组</returns>
@@ -546,7 +565,8 @@ namespace HslCommunication.BasicFramework
         #region Byte Reverse By Word
 
         /// <summary>
-        /// 将byte数组按照双字节进行反转，如果为单数的情况，则自动补齐
+        /// 将byte数组按照双字节进行反转，如果为单数的情况，则自动补齐 ->
+        /// Reverses the byte array by double byte, or if the singular is the case, automatically
         /// </summary>
         /// <param name="inBytes">输入的字节信息</param>
         /// <returns>反转后的数据</returns>
@@ -573,7 +593,8 @@ namespace HslCommunication.BasicFramework
         #region Byte[] and AsciiByte[] transform
 
         /// <summary>
-        /// 将原始的byte数组转换成ascii格式的byte数组
+        /// 将原始的byte数组转换成ascii格式的byte数组 ->
+        /// Converts the original byte array to an ASCII-formatted byte array
         /// </summary>
         /// <param name="inBytes">等待转换的byte数组</param>
         /// <returns>转换后的数组</returns>
@@ -583,7 +604,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 将ascii格式的byte数组转换成原始的byte数组
+        /// 将ascii格式的byte数组转换成原始的byte数组 ->
+        /// Converts an ASCII-formatted byte array to the original byte array
         /// </summary>
         /// <param name="inBytes">等待转换的byte数组</param>
         /// <returns>转换后的数组</returns>
@@ -599,7 +621,8 @@ namespace HslCommunication.BasicFramework
 
 
         /// <summary>
-        /// 将bool数组转换到byte数组
+        /// 将bool数组转换到byte数组 ->
+        /// Converting a bool array to a byte array
         /// </summary>
         /// <param name="array">bool数组</param>
         /// <returns>转换后的字节数组</returns>
@@ -639,7 +662,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 从Byte数组中提取位数组
+        /// 从Byte数组中提取位数组，length代表位数 ->
+        /// Extracts a bit array from a byte array, length represents the number of digits
         /// </summary>
         /// <param name="InBytes">原先的字节数组</param>
         /// <param name="length">想要转换的长度，如果超出自动会缩小到数组最大长度</param>
@@ -688,7 +712,8 @@ namespace HslCommunication.BasicFramework
         #region Byte[] Splice
 
         /// <summary>
-        /// 拼接2个字节数组的数据
+        /// 拼接2个字节数组成一个数组 ->
+        /// Splicing 2 bytes to to an array
         /// </summary>
         /// <param name="bytes1">数组一</param>
         /// <param name="bytes2">数组二</param>
@@ -709,7 +734,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 将一个byte数组的前面指定位数移除，返回新的一个数组
+        /// 将一个byte数组的前面指定位数移除，返回新的一个数组 ->
+        /// Removes the preceding specified number of bits in a byte array, returning a new array
         /// </summary>
         /// <param name="value">字节数组</param>
         /// <param name="length">等待移除的长度</param>
@@ -723,7 +749,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 将一个byte数组的后面指定位数移除，返回新的一个数组
+        /// 将一个byte数组的后面指定位数移除，返回新的一个数组 ->
+        /// Removes the specified number of digits after a byte array, returning a new array
         /// </summary>
         /// <param name="value">字节数组</param>
         /// <param name="length">等待移除的长度</param>
@@ -737,7 +764,8 @@ namespace HslCommunication.BasicFramework
         }
 
         /// <summary>
-        /// 将一个byte数组的前后移除指定位数，返回新的一个数组
+        /// 将一个byte数组的前后移除指定位数，返回新的一个数组 ->
+        /// Removes a byte array before and after the specified number of bits, returning a new array
         /// </summary>
         /// <param name="value">字节数组</param>
         /// <param name="leftLength">前面的位数</param>
@@ -762,7 +790,8 @@ namespace HslCommunication.BasicFramework
         #region Basic Framework
 
         /// <summary>
-        /// 设置或获取系统框架的版本号
+        /// 设置或获取系统框架的版本号 ->
+        /// Set or get the version number of the system framework
         /// </summary>
         /// <remarks>
         /// 当你要显示本组件框架的版本号的时候，就可以用这个属性来显示
@@ -775,7 +804,8 @@ namespace HslCommunication.BasicFramework
         #region Deep Clone
 
         /// <summary>
-        /// 使用序列化反序列化深度克隆一个对象，该对象需要支持序列化特性
+        /// 使用序列化反序列化深度克隆一个对象，该对象需要支持序列化特性 ->
+        /// Cloning an object with serialization deserialization depth that requires support for serialization attributes
         /// </summary>
         /// <param name="oringinal">源对象，支持序列化</param>
         /// <returns>新的一个实例化的对象</returns>
@@ -809,7 +839,8 @@ namespace HslCommunication.BasicFramework
         #region Unique String Get
 
         /// <summary>
-        /// 获取一串唯一的随机字符串，长度为20，由Guid码和4位数的随机数组成，保证字符串的唯一性
+        /// 获取一串唯一的随机字符串，长度为20，由Guid码和4位数的随机数组成，保证字符串的唯一性 ->
+        /// Gets a string of unique random strings with a length of 20, consisting of a GUID code and a 4-digit random number to guarantee the uniqueness of the string
         /// </summary>
         /// <returns>随机字符串数据</returns>
         /// <example>
